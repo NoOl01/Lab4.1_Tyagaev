2bfd1575febf66eeea0b9341eaba290c98e06a41
Anton <Anton@DESKTOP-6RE5PBQ>
2023-11-16 00:05:41 +0200

    Добавьте файлы проекта.

---
+++ Lab4.1_Tyagaev/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Write("Введите размер массива: ");
+int n = int.Parse(Console.ReadLine());
+int[] mas = new int[n];
+Random rnd = new Random();
+for (int i = 0; i < mas.Length; i++)
+{
+    mas[i] = rnd.Next(0, 2);
+    Console.Write(mas[i] + " ");
+}
+Console.WriteLine();
+
+double sum = 0;
+for (int i = 0; i < mas.Length; i++)
+{
+    sum += mas[i] * Math.Pow(2, mas.Length - i - 1);
+}
+int decimalSum = Convert.ToInt32(sum);
+int octal = 0;
+int counter = 0;
+while (decimalSum != 0)
+{
+    int remainder = decimalSum % 8;
+    decimalSum /= 8;
+    octal += remainder * (int)Math.Pow(10, counter);
+    counter++;
+}
+
+Console.WriteLine($"Полученное восьмеричное число: {octal}");
